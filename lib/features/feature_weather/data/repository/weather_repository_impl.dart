7ffcd0c47d464b4b0609710cf5cb939a0c10f5f3
Mahdiyar Arbabzi <m.arbabzi@gmail.com>
2024-01-12 19:29:35 +0330

    feat: implement weather repository design pattern

---
+++ lib/features/feature_weather/data/repository/weather_repository_impl.dart
@@ -0,0 +1,30 @@
+import 'package:dio/dio.dart';
+import 'package:weather_app/core/resources/data_state.dart';
+import 'package:weather_app/features/feature_weather/data/data_source/remote/api_provider.dart';
+import 'package:weather_app/features/feature_weather/data/models/current_city_model.dart';
+import 'package:weather_app/features/feature_weather/domain/entities/current_city_entity.dart';
+import 'package:weather_app/features/feature_weather/domain/repository/weather_repository.dart';
+
+class WeatherRepositoryImpl extends WeatherRepository {
+  ApiProvider apiProvider;
+  WeatherRepositoryImpl(this.apiProvider);
+
+  @override
+  Future<DataState<CurrentCityEntity>> fetchCurrentWeatherData(
+      String cityName) async {
+    try {
+      final Response response = await apiProvider.callCurrentWeather(cityName);
+
+      if (response.statusCode == 200) {
+        final CurrentCityEntity currentCityEntity =
+            CurrentCityModel.fromJson(response.data);
+
+        return DataSuccess(currentCityEntity);
+      } else {
+        return const DataFailed('Something went wrong, try again...');
+      }
+    } catch (e) {
+      return const DataFailed('Please check your connection...');
+    }
+  }
+}
